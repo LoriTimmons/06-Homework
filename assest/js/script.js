7eee6e5bbe17019590b24062e44baddfe6cac4b7
Lori Timmons <lorietimmons@gmail.com>
2021-09-19 20:45:53 -0500

    updated code

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,37 +1,57 @@
 const weatherApiRootUrl = 'https://api.openweathermap.org';
 const weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
+// Loris edge cases 
+// --Box that changes colors for UVI
+// --Weather icons (font awesome)
+// --Date format (tricky) Search how to find current date. Set and add one "++". moments? append to right place.
+//--does search history stay between refreshes 
 
 function displayCurrent(current) {
-//Use jQuery to add to here:
-// <section
-   //           id="today"
-     //       ></section>
-// loris PSC
-//     -Create 
-//     -create UL elements to make up todays weather list temp/wind/humd/UV
-//     -append to #today-weather-info ul 
-//     -get info for #searchTerm and append it to #today
+  // where does this go!?🚫 
+  const currentWeather = $("#today");
+  console.log($("#today"));
+  console.log(current);
+  // template lit -Lori Look up. Added in vars easy. Just look up to def. Already using them 👀
+  currentWeather.append(`<p>Temp ${current.temp}<p>`);
+  currentWeather.append(`<p>Wind ${current.wind_speed}<p>`);
+  currentWeather.append(`<p>Humidity ${current.humidity}<p>`);
+  // UV Index need to add class to make the box change colors 🚫
+  currentWeather.append(`<p>UV Index: ${current.uvi}<p>`);
 }
 
 function displayFiveDay(daily) {
+  console.log(daily);
+  // Ex on CL for this section append. JQ get ID and then append things
+  // refactor to for loop 🚫
+  console.log(daily[0].temp);
+  const fiveDayWeather = $("#forecast");
+  fiveDayWeather.append(`<p>Temp ${daily[0].temp}<p>`);
 //Use jQuery to add to here:
 // <section
    //           id="forecast"
      //       ></section>
 }
 
-// have bill go over
+
 function saveToLocalState(city) {
+  console.log(city);
+  const history = $("#history");
+  history.append(`<p>${city}<p>`);
 
 }
 
 // Have bill go over
 function searchCityWeather() {
   const city = document.querySelector('#searchTerm').value;
+  // fetch and getting a long string 
+  console.log(`${weatherApiRootUrl}/geo/1.0/direct?q=${city}&limit=5&appid=${weatherApiKey}`);
   fetch(`${weatherApiRootUrl}/geo/1.0/direct?q=${city}&limit=5&appid=${weatherApiKey}`)
-  .then(function (res) {
-    return res.json();
+  // getting the info back from the API and telling the site what to do
+  .then(function (response) {
+    console.log(response);
+    return response.json();
   })
+  //rtn'ing search results 
   .then(function (body) {
     console.log('body', body);
     const lat = body[0].lat;
@@ -42,10 +62,11 @@ function searchCityWeather() {
   .then(function (res) {
     return res.json();
   })
-  .then(function (body) {
-    console.log(body)
-    const current = body.current;
-    const daily = body.daily;
+  // 5 day forecast
+  .then(function (bodyResponse) {
+    console.log(bodyResponse)
+    const current = bodyResponse.current;
+    const daily = bodyResponse.daily;
     displayCurrent(current);
     displayFiveDay(daily);
     saveToLocalState(city);
